856869777408f8c6178ea01b44563eb62c37880f
Kutakka <daisoujou@gmail.com>
2025-04-20 10:42:25 -0400

    basic player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -7,13 +8,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0,0,0)
+    dt = 0
+    clock = pygame.time.Clock()
+    player_1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        
+        screen.fill("black")
+        player_1.update(dt)
+        player_1.draw(screen)
         pygame.display.flip()
-    
+        clock.tick(60)
+        dt = (clock.tick(60)/1000)
+
+
 if __name__ == "__main__":
     main()
